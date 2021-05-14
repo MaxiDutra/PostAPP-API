e52537304ba0882e7dd53189b22794eff3983747
Maxi Dutra <m.dutra2211@gmail.com>
2021-05-14 17:18:26 -0300

    inital commit

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import express from 'express';
+import morgan from 'morgan';
+
+class Server {
+
+public app : express.Application;
+
+    constructor(){
+        this.app = express();
+        this.config();
+    }
+    config(){
+       this.app.set('port', process.env.PORT || 3000);
+       this.app.use(morgan('dev'));
+    }
+    routes(){
+
+    }
+    start(){
+        this.app.listen(this.app.get('port'), () =>{
+            console.log('Server on port ', this.app.get('port'));
+        });
+    }
+
+
+}
+
+const server = new Server();
+server.start();
