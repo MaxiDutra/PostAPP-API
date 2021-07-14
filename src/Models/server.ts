90b493c86fb82a95065a53aac7ef0957b9fef0a0
Maxi Dutra <m.dutra2211@gmail.com>
2021-07-14 17:25:50 -0300

    fix: Mejora los mensajes concatenados

--- src/Models/server.ts
+++ src/Models/server.ts
@@ -2,19 +2,29 @@ import express, {Application} from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
 
+import UserRoutes from '../Routes/UserRoutes';
+
 class Server {
 
     private app : Application;
     private port : string;
+    private apiPaths = {
+        usuarioPaths: '/api/user'
+    }
 
     constructor() {
         this.app = express();
         this.port = process.env.PORT || '3000';
+        this.routes();
+    }
+
+    routes(){
+        this.app.use(this.apiPaths.usuarioPaths, UserRoutes)
     }
 
     listen(){
         this.app.listen(this.port, () =>{
-            console.log('Server on port: '+ this.port);
+            console.log(`\nServer on port: ${this.port}`);
         });
     }
 
