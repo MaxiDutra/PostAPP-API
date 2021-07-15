59dd1ca4b2d10e98b8699933466dffc8fa5642e7
Maxi Dutra <m.dutra2211@gmail.com>
2021-07-15 00:24:32 -0300

    feat: crea metodo de acceso a la conección desde el servidor

---
+++ src/Models/server.ts
@@ -0,0 +1,47 @@
+import express, {Application} from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+
+import UserRoutes from '../Routes/UserRoutes';
+import dbConnection from '../Database/config';
+
+class Server {
+
+    private app : Application;
+    private port :  string;
+    private apiPaths = {
+        usuarioPaths: '/api/user'
+    }
+
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT || '3000';
+        this.routes();
+        this.connectDB();
+    }
+
+    routes(){
+        this.app.use(this.apiPaths.usuarioPaths, UserRoutes)
+    }
+
+    listen(){
+        this.app.listen(this.port, () =>{
+            console.log(`\nServer on port: ${this.port}`);
+        });
+    }
+
+    middlewares(){
+        this.app.use(express.json);
+        this.app.use(morgan('dev'));
+        this.app.use(cors());
+    }
+
+    // De esta forma podemos especificar si queremos tener diferentes cadenas de conexiónes
+    // dependiendo de la variable de entorno 
+    async connectDB(){
+        await dbConnection();
+    }
+
+}
+
+export default Server;
